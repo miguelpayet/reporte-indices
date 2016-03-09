5a7b558bd53e2af3c9d9d7bdbb5a46b2667c983f
miguelpayet <miguelpayet@gmail.com>
2016-03-09 16:34:13 -0500

    Esta clase ahora tiene la lógica para ejecutar la consulta varias veces en distintos hilos.

--- src/main/java/adaptador/CuentaDatabase.java
+++ src/main/java/adaptador/CuentaDatabase.java
@@ -1,31 +1,87 @@
 package adaptador;
 
+import config.ConfiguracionException;
+import excel.Excel;
+import excel.ExcelException;
+import main.Consultador;
+import main.ConsultadorAplicacion;
+import main.ExcepcionConsultador;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CuentaDatabase {
 
-	String host;
-	String password;
-	String service;
-	String usuario;
+	private Consulta consulta;
+	private Database db;
+	private String host;
+	private String password;
+	private String service;
+	private String usuario;
 
-	public CuentaDatabase() {
+	public CuentaDatabase() throws ConfiguracionException {
 	}
 
-	@SuppressWarnings("unused")
-	public String getHost() {
+	public void ejecutarConsultas() {
+		try {
+			ArrayList<Thread> threads = new ArrayList<>();
+			Excel excel = new Excel(ConsultadorAplicacion.getConfiguracion().leerExcel());
+			db = new Database(this);
+			db.connect();
+			List<Condicion> condiciones = consulta.getCondiciones();
+			try {
+				for (Condicion cond : condiciones) {
+					ConsultadorAplicacion.getLogger().info("hoja: " + cond.getHoja());
+					Consultador lector = new Consultador(db.getConnection(), consulta.getQuery(), cond.getCondicion());
+					lector.setExcel(excel.getCurrentSheet(cond.getHoja()));
+					Thread thread = new Thread(lector);
+					threads.add(thread);
+					thread.start();
+				}
+				for (Thread thread : threads) {
+					try {
+						thread.join();
+					} catch (InterruptedException e) {
+						ConsultadorAplicacion.getLogger().info(String.format("Thread interrumpido: %s", e.getMessage()));
+					}
+				}
+			} finally {
+				ConsultadorAplicacion.getLogger().info("uniendo threads");
+				ConsultadorAplicacion.getLogger().info("grabar excel");
+				excel.grabar();
+			}
+		} catch (ConfiguracionException e) {
+			throw new RuntimeException("Error al leer configuración en ejecutarConsultas");
+		} catch (DatabaseException e) {
+			ConsultadorAplicacion.getLogger().error("error al conectar a bd en ejecutarConsultas: " + e.getMessage());
+		} catch (ExcelException e) {
+			ConsultadorAplicacion.getLogger().error("error al grabar excel en ejecutarConsultas: " + e.getMessage());
+		} catch (ExcepcionConsultador e) {
+			ConsultadorAplicacion.getLogger().error("error en ejecutarConsultas: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			db.close();
+		}
+	}
+
+	String getHost() {
 		return host;
 	}
 
-	public String getPassword() {
+	String getPassword() {
 		return password;
 	}
 
-	@SuppressWarnings("unused")
-	public String getService() {
+	String getService() {
 		return service;
 	}
 
-	public String getUsuario() {
+	String getUsuario() {
 		return usuario;
+	}
+
+	public void setConsulta(Consulta consulta) {
+		this.consulta = consulta;
 	}
 
 	public void setHost(String host) {
