dfee1a0446581331ab20b2b71826ce56700af77d
miguelpayet <miguelpayet@gmail.com>
2016-03-11 12:52:55 -0500

    añadir dos opciones en línea de comando para multihilo y para el intervalo en minutos del informe de avance

--- src/main/java/main/ConsultadorAplicacion.java
+++ src/main/java/main/ConsultadorAplicacion.java
@@ -3,6 +3,7 @@ package main;
 import adaptador.CuentaDatabase;
 import config.Configuracion;
 import config.ConfiguracionException;
+import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,6 +14,17 @@ public class ConsultadorAplicacion {
 	private static Configuracion configuracion = null;
 	private static final Logger logger = LogManager.getLogger(ConsultadorAplicacion.class);
 	private static ArrayList<CuentaDatabase> cuentas;
+	private static Long intervaloLog;
+	private static Boolean usarHilos;
+
+	private static Options crearOpciones() {
+		Options options = new Options();
+		Option optHilos = Option.builder("m").longOpt("multihilo").required(false).hasArg(true).type(Long.class).desc("usar un hilo por consulta (1 = si, 0 = no)").build();
+		options.addOption(optHilos);
+		Option optIntervalo = Option.builder("i").longOpt("intervalo-log").required(false).hasArg(true).type(Boolean.class).desc("minutos de intervalo de impresión de avance").build();
+		options.addOption(optIntervalo);
+		return options;
+	}
 
 	public static Configuracion getConfiguracion() throws ConfiguracionException {
 		if (configuracion == null) {
@@ -21,12 +33,34 @@ public class ConsultadorAplicacion {
 		return configuracion;
 	}
 
+	static Long getIntervaloLog() {
+		return intervaloLog;
+	}
+
 	public static Logger getLogger() {
 		return logger;
 	}
 
 	private static void init(String[] args) throws ConfiguracionException {
 		cuentas = ConsultadorAplicacion.getConfiguracion().leerCuentas();
+		initOpciones(args);
+	}
+
+	private static void initOpciones(String[] args) throws ConfiguracionException {
+		Options options = crearOpciones();
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			intervaloLog = Long.parseLong(cmd.getOptionValue("i", "1"));
+			String opcHilos = cmd.getOptionValue("m", "0");
+			usarHilos = !opcHilos.equals("0");
+		} catch (ParseException e) {
+			throw new ConfiguracionException(String.format("error en parámetros %s", e.getMessage()), e);
+		}
+	}
+
+	public static Boolean isUsarHilos() {
+		return usarHilos;
 	}
 
 	private static void lanzarCuentas() {
