17f07d86dcca24b19e9fbd616dd7b04414c2b666
miguelpayet <miguelpayet@gmail.com>
2016-03-18 09:59:15 -0500

    agregar el nombre de la condición al archivo que se está generando

--- src/main/java/excel/Excel.java
+++ src/main/java/excel/Excel.java
@@ -1,20 +1,41 @@
 package excel;
 
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Excel {
 
 	private ExcelConfig cfg;
-	private Workbook wb;
+	private String nombreCondicion;
+	private SXSSFWorkbook wb;
 
 	public Excel(ExcelConfig cfg) throws ExcelException {
 		this.cfg = cfg;
 		wb = new SXSSFWorkbook(100);
+	}
+
+	public Excel(ExcelConfig unaConfiguracion, String unNombreCondicion) throws ExcelException {
+		this.cfg = unaConfiguracion;
+		this.nombreCondicion = unNombreCondicion;
+		wb = new SXSSFWorkbook(100);
+	}
+
+	@SuppressWarnings("unused")
+	public void abrir() throws ExcelException {
+		try {
+			File file = new File(getFilename());
+			FileInputStream fis = new FileInputStream(file);
+			XSSFWorkbook tempwb = new XSSFWorkbook(fis);
+			wb = new SXSSFWorkbook(tempwb);
+		} catch (IOException e) {
+			throw new ExcelException("grabar - " + e.getClass().getName() + ": " + e.getMessage(), e);
+		}
 	}
 
 	public Sheet getCurrentSheet(String sName) {
@@ -25,11 +46,16 @@ public class Excel {
 		return currentSheet;
 	}
 
+	private String getFilename() {
+		return cfg.getRuta() + cfg.getNombreArchivo().replace(".", String.format("-%s.", nombreCondicion));
+	}
+
 	public void grabar() throws ExcelException {
 		try {
-			FileOutputStream out = new FileOutputStream(cfg.getRuta() + cfg.getNombreArchivo());
+			FileOutputStream out = new FileOutputStream(getFilename());
 			wb.write(out);
 			out.close();
+			wb.close();
 		} catch (IOException e) {
 			throw new ExcelException("grabar - " + e.getClass().getName() + ": " + e.getMessage(), e);
 		}
